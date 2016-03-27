5972f6afb6bbb15fae9f24de4c3e66effd9e6b8c
oi7 <oi7@mit.edu>
2016-03-26 23:07:07 -0400

    Implemented switching between UICamera and AntCamera

--- Assets/Scripts/CameraSwap.cs
+++ Assets/Scripts/CameraSwap.cs
@@ -13,19 +13,31 @@ public class CameraSwap : MonoBehaviour
 {
 	public GameObject UICamera;
 	public GameObject ARCamera;
+	public GameObject AntCamera;
 
 	void Start() {
 		UICamera.SetActive(true);
 		ARCamera.SetActive(false);
+		ARCamera.SetActive(false);
 	}
 
-	public void Switch() {
+	public void SwitchAR() {
 		if (UICamera.activeSelf == true) {
 			UICamera.SetActive (false);
 			ARCamera.SetActive (true);
 		} else {
 			UICamera.SetActive (true);
 			ARCamera.SetActive (false);
+		}
+	}
+
+	public void SwitchAnt() {
+		if (UICamera.activeSelf == true) {
+			UICamera.SetActive (false);
+			AntCamera.SetActive (true);
+		} else {
+			UICamera.SetActive (true);
+			AntCamera.SetActive (false);
 		}
 	}
 }

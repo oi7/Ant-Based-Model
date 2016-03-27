09271282a282078387b593866d0b96ed53583bda
oi7 <oi7@mit.edu>
2016-03-26 21:22:25 -0400

    Placed 3 types of ants in the main scene and configure their walking animations

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,29 @@
+﻿/// Ant-Based Modeling
+/// 
+/// Developed and Designed by Poseidon Ho at MIT Media Lab 
+/// 
+/// Script for controlling agent's movements
+/// 
+
+using UnityEngine;
+using System.Collections;
+
+public class AgentController : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			animator.SetTrigger("Walking");
+			Debug.Log("walk");
+		}
+	}
+}
